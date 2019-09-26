fcf10af69cd09f48f85a637e38320841ecd142a3
shrutidhananjaypatil <shrutidpatil12@gmail.com>
2019-09-26 12:02:54 +0530

    [Shruti] Add . to compute exponent of positive number

--- src/test/java/com/thoughtworks/bootcamp/PowerOfTwoTest.java
+++ src/test/java/com/thoughtworks/bootcamp/PowerOfTwoTest.java
@@ -6,9 +6,22 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class PowerOfTwoTest {
     @Test
-    void givenNumberAsTwo_WhenComputePowerOfZero_ThenShouldReturnOne() {
+    void givenNumberAsTwo_WhenCalculatePower_ThenShouldReturnOne() {
         PowerOfTwo power = new PowerOfTwo();
         assertEquals(1, power.compute(0));
     }
+
+    @Test
+    void givenPositiveNumber_WhenCalculatePower_ThenShouldReturnOne() {
+        PowerOfTwo power = new PowerOfTwo();
+        assertEquals(4, power.compute(2));
+    }
+
+    @Test
+    void givenPositiveNumber_WhenCalculatePower_ThenShouldReturnTwo() {
+        PowerOfTwo power = new PowerOfTwo();
+        assertEquals(16, power.compute(4));
+    }
+
 
 }
